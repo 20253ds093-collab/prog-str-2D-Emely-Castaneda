691562f5ab46c8b686a7687967ce5a91d904ba45
dxemely <20253ds093@utez.edu.mx>
2026-02-14 21:18:47 -0600

    Nuevos cambios

--- Alumnoscrudreportes/src/App.java
+++ Alumnoscrudreportes/src/App.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-public class App import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
